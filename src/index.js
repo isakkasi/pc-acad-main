4617290acba3c8f6e98e91a4c2e41c70d42f3779
isakkasi <83792153+isakkasi@users.noreply.github.com>
2022-06-12 19:56:27 +0300

    Update the main site

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+// const main = require('./views/layouts/index')
 
 const app = express();
 
+// set up the handlebars
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+// set up the static files
+app.use('/static', express.static('public'));
+
 app.get('/', (req, res) => {
-     res.send('Bravoooo')
+    res.render('webpage')
 })
 
-app.listen()
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is started ............................................................');
+})

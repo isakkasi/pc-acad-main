8a0fa9e49bd44b8eb015c97305bcc0ba2f021c49
isakkasi <83792153+isakkasi@users.noreply.github.com>
2022-06-12 19:59:53 +0300

    Contact us form can post data

--- src/index.js
+++ src/index.js
@@ -12,8 +12,15 @@ app.set('views', './src/views');
 // set up the static files
 app.use('/static', express.static('public'));
 
+//Middleware for reading the body of the POST requests
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.render('webpage')
+})
+
+app.post('/', (req, res) => {
+     console.log(req.body);
 })
 
 app.listen(process.env.PORT || 5000, () => {

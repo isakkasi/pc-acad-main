2c2a83c5e31db6a16257785b5563c3ba19f32f35
isakkasi <83792153+isakkasi@users.noreply.github.com>
2022-06-13 09:24:26 +0300

    Send mail is working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const mail = require('nodemailer')
 // const main = require('./views/layouts/index')
 
 const app = express();
@@ -15,12 +16,35 @@ app.use('/static', express.static('public'));
 //Middleware for reading the body of the POST requests
 app.use(express.urlencoded({ extended: false }));
 
+//Configure mail
+let mailConfig ={
+    host: 'pc-acad.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'no-replay@pc-acad.com',
+        pass: 'noreplaypc-acad'
+    }
+}
+const transporter = mail.createTransport(mailConfig)
+
+let mailContent = {
+    from: 'no-replay@pc-acad.com',
+    to: 'a.georgiev@plane-care.com',
+    subject: 'Test',
+    text: 'Hello',
+}
+
 app.get('/', (req, res) => {
     res.render('webpage')
 })
 
 app.post('/', (req, res) => {
+    console.log('Something');
      console.log(req.body);
+     transporter.sendMail(mailContent)
+     .then((info) => console.log(info))
+     .catch(err => {console.log(err)})
 })
 
 app.listen(process.env.PORT || 5000, () => {
